b6975de314233a53681a709d7430bdf6e4ed2693
“Divyang <divyang@simpletire.com>
2018-04-04 03:27:57 +0530

    Working Todo Interface using react Components

--- todo/src/index.js
+++ todo/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
